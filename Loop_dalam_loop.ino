4c86582feabc0a152b78a24f4deefb9c98f0befa
zawawizek <33506080+zawawizek@users.noreply.github.com>
2017-11-13 21:52:34 +0800

    Add files via upload

---
+++ Loop_dalam_loop.ino
@@ -0,0 +1,18 @@
+//loop dalam loop
+
+void setup() {
+Serial.begin(9600);
+}
+
+void loop() {
+  for(int x =0; x<5; x++)
+{
+  for(int r =0; r<5; r++)
+  {
+    Serial.println(r);
+    delay(2000);
+  }
+Serial.println(x);
+}
+}
+
