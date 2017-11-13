628691e254c3f73c2dd010c8995bd5d39e2b90ec
MinAznan <Aminaaaznan@gmail.com>
2017-11-13 23:09:35 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,133 @@
+﻿<!DOCTYPE html>
+
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>Meaple Delight</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+<body>
+	<div id="header">
+		<div>
+			<div>
+				<div id="logo">
+					<a href="index.php"><img src="images/logo.gif" alt="Logo"/></a>
+				</div>
+				<div>
+					<div>
+						<a href="signup.php">My Account</a>
+						<a href="index.php">Help</a>
+						<a href="signin.php" class="last">Sign in</a>
+					</div>
+				
+				</div>
+			</div>
+			<ul>
+				<li class="current"><a href="index.php">HOME</a></li>
+				<li><a href="about.php">ABOUT US</a></li>
+				<li><a href="cake.php">CAKES</a></li>
+				<li><a href="promo.php">PROMOTIONS</li>
+				<li><a href="occassions.php">OCCASSIONS</li>
+				<li><a href="contact.php">CONTACT US</a></li>
+			</ul>
+			<div id="section">
+				
+					
+				<style>
+		body {font-family:Arial, Helvetica, sans-serif; font-size:12px;}
+		 
+		.fadein { 
+		position:relative; height:300px; width:500px; margin:0 auto;
+		background: url("slideshow-bg.png") repeat-x scroll left top transparent;
+		padding: 10px 450px 450px;
+		 }
+		.fadein img { position:absolute; left:10px; top:10px; }
+</style>
+ 
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+<script>
+$(function(){
+	$('.fadein img:gt(0)').hide();
+	setInterval(function(){$('.fadein :first-child').fadeOut().next('img').fadeIn().end().appendTo('.fadein');}, 3000);
+});
+</script>
+ 
+</head>
+<body>
+<div class="fadein">
+		<img src="http://data.whicdn.com/images/62878620/large.png" width="830" height="350" >
+		<img src="http://imgfave-herokuapp-com.global.ssl.fastly.net/image_cache/1319131340510663.jpg" width="830" height="350">
+		<img src="http://images.huffingtonpost.com/2015-07-29-1438194128-3791769-national_cheesecake_day.jpg" width="830" height="350">
+		<img src="http://s5.favim.com/orig/74/lt3-beautiful-cupcakes-food-Favim.com-772177.jpg" width="830" height="350">
+		<img src="http://www.yummly.com/wp-content/uploads/2012/05/No-Bake-Mango-Cheesecake-with-Mango-Sauce.png" width="830" height="350">
+		<img src="https://prinsipenghangin.files.wordpress.com/2010/12/dsc01774.jpg" width="830" height="350">
+		<img src="http://static.tumblr.com/9a3736c3756e83bfa0fd82f57fbfd95e/qgr1igg/vDHnxkrfu/tumblr_static_2qhz7t169aqss444goskg8wgk.jpg" width="830" height="350">
+		<img src="http://favim.com/orig/201106/09/chcolate-chip-muffin-chocolate-delicious-food-muffins-yum-Favim.com-69553.jpg" width="830" height="350">
+</div>
+			
+            
+            
+			</div>
+		</div>
+	</div>
+	<div id="content">
+		<div class="home">
+			<div class="aside">
+				<h1>WELCOME !!</h1>
+				<p>Pastry is the name given to various kinds of baked goods made from ingredients such as flour, butter, shortening, baking powder or eggs. Small cakes, tarts and other sweet baked goods are called &#34;pastries&#34;.</p> 
+				<p>Pastry may also refer to the dough from which such baked goods are made. Pastry dough is rolled out thinly and used as a base for baked goods. Common pastry dishes include pies, tarts and quiches. 
+				<p>Pastry is distinguished from bread by having a higher fat content, which contributes to a flaky or crumbly texture. A good pastry is light and airy and fatty, but firm enough to support the weight of the filling. When making a shortcrust pastry, care must be taken to blend the fat and flour thoroughly before adding. <a href="index.html" class="readmore">read more</a></p>
+			</div>
+			<div class="section">
+				<div>
+					<h2>New Arrival</h2>
+					<p>Cakes are our passion! Did you try the amazing sweet temptations of Meaple Delight Cafe? You can choose your cake from our catalog. We offer wedding cakes, children’s cakes and cakes for all your occasions. Check out for our new arrival cake and have a taste! <br>Our latest cakes on the wheel are CAPPUCINO & TIRAMISU square cakes! </p>
+				</div>
+				<ul>
+					<li class="first">
+						<a href="index.php"><img src="images/new1.png" alt="Image" /></a>
+					</li>
+					<li>
+						<a href="index.php"><img src="images/new2.png" alt="Image" /></a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div id="footer">
+		<div class="section">
+			<div>
+				<div class="aside">
+					<div>
+						<div>
+							<b>Too <span>BUSY</span> to shop?</b>
+							<a href="signup.php">Sign up for free</a>
+							<p>and we&#39;ll deliver it on your doorstep</p>
+						</div>
+					</div>
+				</div>
+			<div class="connect">
+					<span>Follow Us</span>
+					<ul>
+						<li><a href="#" target="_blank" class="facebook">Facebook</a></li>
+						<li><a href="#" target="_blank" class="twitter">Twitter</a></li>
+						<li><a href="#" class="subscribe">Subscribe</a></li>
+						<li><a href="#" target="_blank" class="flicker">Flicker</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div id="navigation">
+			<div>
+				<ul>
+					<li class="first"><a href="index.php">help</a></li>
+					<li><a href="index.php">about cake delight</a></li>
+					<li><a href="index.php">privacy policy</a></li>
+					<li><a href="index.php">terms of use</a></li>
+				</ul>
+				<p>Copyright &copy; 2016 cake delight  All rights reserved</p>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
